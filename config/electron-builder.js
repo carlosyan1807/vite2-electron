05241bab152e9a538383ff82b45e17341d0d24f8
UncleCarlos <50797685+UncleCarlos@users.noreply.github.com>
2021-01-13 10:50:55 +0800

    Initial commit

---
+++ config/electron-builder.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @type {import('electron-builder').Configuration}
+ */
+module.exports = {
+  directories: {
+    output: 'dist/app',
+    buildResources: 'build',
+    app: 'dist/source',
+  },
+};
---
+++ config/renderer.vite.js
@@ -0,0 +1,24 @@
+const {join} = require('path');
+const vue = require('@vitejs/plugin-vue');
+const {chrome} = require('./electron-dep-versions');
+/**
+ * @type {import('vite').UserConfig}
+ */
+module.exports = {
+  root: join(process.cwd(), './src/renderer'),
+  alias: {
+    '/@/': join(process.cwd(), './src/renderer') + '/',
+  },
+  plugins: [vue()],
+  build: {
+    target: `chrome${chrome}`,
+    polyfillDynamicImport: false,
+    base: '',
+    outDir: '../../dist/source/renderer',
+    assetsDir: '.',
+    rollupOptions: {
+      external: require('./external-packages').default,
+    },
+  },
+};
+
